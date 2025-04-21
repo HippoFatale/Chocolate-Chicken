19ca1e203f53680e53d4bb4e1e8ffdb6211c2805
hippofatale <hippo7665@gmail.com>
2025-04-21 20:27:37 +0900

    Added Chocolate Syrup recipes.

--- src/main/java/hippofatale/chocolatechickenmod/ChocolateChickenMod.java
+++ src/main/java/hippofatale/chocolatechickenmod/ChocolateChickenMod.java
@@ -1,6 +1,7 @@
 package hippofatale.chocolatechickenmod;
 
 import hippofatale.chocolatechickenmod.block.ModBlocks;
+import hippofatale.chocolatechickenmod.item.ModCreativeModeTabs;
 import hippofatale.chocolatechickenmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -41,6 +42,8 @@ public class ChocolateChickenMod
         // Note that this is necessary if and only if we want *this* class (ChocolateChickenMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/hippofatale/chocolatechickenmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,51 @@
+package hippofatale.chocolatechickenmod.item;
+
+import hippofatale.chocolatechickenmod.ChocolateChickenMod;
+import hippofatale.chocolatechickenmod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ChocolateChickenMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> CHOCOLATE_CHICKEN_ITEMS_TAB = CREATIVE_MODE_TAB.register("chocolate_chicken_items_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.CHOCOLATE_SYRUP.get()))
+                    .title(Component.translatable("creativetab.chocolatechickenmod.chocolate_chicken_items"))
+                    .displayItems((parameters, output) -> {
+                        //items
+                        output.accept(ModItems.CHOCOLATE_SYRUP);
+
+                        //blocks
+                        output.accept(ModBlocks.CHOCOLATE_SYRUP_CRATE);
+
+                    })
+
+                    .build());
+
+//    public static final Supplier<CreativeModeTab> BLUE_LEMON_ITEMS_TAB = CREATIVE_MODE_TAB.register("blue_lemon_items_tab",
+//            () -> CreativeModeTab.builder()
+//                    .icon(() -> new ItemStack(ModItems.CHOCOLATE_SYRUP.get()))
+//                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ChocolateChickenMod.MOD_ID, "chocolate_chicken_items_tab"))
+//                    .title(Component.translatable("creativetab.chocolatechickenmod.blue_lemon_items"))
+//                    .displayItems((parameters, output) -> {
+//                        //items
+//
+//                        //blocks
+//
+//                    })
+//
+//                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+}
